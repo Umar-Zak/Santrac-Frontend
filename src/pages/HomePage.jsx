d9b0abc7d4b652de5e89ce7af90581e3ab3ac8ad
umar-zak <umarabanga78@gmail.com>
2021-07-15 01:36:22 -0800

    add utils

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,67 +1,109 @@
-import React from 'react';
-import {FaFlag,FaBuilding,FaTools,FaHouseDamage} from "react-icons/fa"
-import Footer from '../component/footer';
-import AboutImage from '../component/image';
-import Mission from '../component/mission';
-import Navbar from '../component/navbar';
-import Product from '../component/product';
-import SectionHeader from '../component/section-header';
-import Service from '../component/service';
-import Slide from '../component/slide';
+import React from "react";
+import { FaFlag, FaBuilding, FaTools, FaHouseDamage } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
+import Footer from "../component/footer";
+import AboutImage from "../component/image";
+import Mission from "../component/mission";
+import Navbar from "../component/navbar";
+import Product from "../component/product";
+import SectionHeader from "../component/section-header";
+import Service from "../component/service";
+import Slide from "../component/slide";
+import { getHotProducts, getHighlightProduct } from "../utils/products";
 
 const HomePage = () => {
-    return (<div className="block">
-        <Navbar />
-        <Slide />
-        <section className="hot-sale-section">
-            <Product   name="Wall Tiles" price="A new wall tiles brand from Italy. Limited stock. Rush while stock last" />
-            <Product   name="Emulsion Paint" price=" 30% Early bird discount for the next 2 weeks" />
-            <Product   name="Azar Oil Paint" price="Buy 3 bucket and get 1 free" />
-            <Product   name="Master Locks" price="Suitable for all security purposes" />
-        </section>
-        <section className="services-section">
-            <SectionHeader text="Know our services" />
-            <p className="services--tagline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, eveniet.</p>
-            <div className="services-container">
-                <Service title="Real estate" text="We have a lot of real estate projects under our belt. We deliver with competence" >
-                    <FaFlag className="service__icon"/>
-                </Service>
-                <Service title="Building and construction" text="We have a lot of real estate projects under our belt. We deliver with competence" >
-                    <FaBuilding className="service__icon"/>
-                </Service>
-                <Service title="sales of building and construction equipment and materials" text="We have a lot of real estate projects under our belt. We deliver with competence" >
-                    <FaTools className="service__icon"/>
-                </Service>
-                <Service title="Architecture" text="We have a lot of real estate projects under our belt. We deliver with competence" >
-                    <FaHouseDamage className="service__icon"/>
-                </Service>
+  const history = useHistory();
+
+  const handleClick = (id) => {
+    history.push(`/product/${id}`);
+  };
+  return (
+    <div className="block">
+      <Navbar />
+      <Slide />
+      <section className="hot-sale-section">
+        {getHotProducts().map((p) => {
+          return (
+            <Product image={p.image} name={p.name} price={p.description} />
+          );
+        })}
+      </section>
+      <section className="services-section">
+        <SectionHeader text="Know our services" />
+        <p className="services--tagline">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+          eveniet.
+        </p>
+        <div className="services-container">
+          <Service
+            title="Real estate"
+            text="We have a lot of real estate projects under our belt. We deliver with competence"
+          >
+            <FaFlag className="service__icon" />
+          </Service>
+          <Service
+            title="Building and construction"
+            text="We have a lot of real estate projects under our belt. We deliver with competence"
+          >
+            <FaBuilding className="service__icon" />
+          </Service>
+          <Service
+            title="sales of building and construction equipment and materials"
+            text="We have a lot of real estate projects under our belt. We deliver with competence"
+          >
+            <FaTools className="service__icon" />
+          </Service>
+          <Service
+            title="Architecture"
+            text="We have a lot of real estate projects under our belt. We deliver with competence"
+          >
+            <FaHouseDamage className="service__icon" />
+          </Service>
         </div>
-        </section>
-        <section className="product-section">
-            <SectionHeader text="Products" />
-            <div className="product-container">
-                <Product name="Iron Rods" price="20.00" showPrice={true}  />
-                <Product name="Binding wire" price="60.00" showPrice={true}  />
-                <Product name="wooden board" price="100.00" showPrice={true}  />
-                <Product name="Tape measure" price="90.00" showPrice={true}  />
-            </div>
-        </section>
-        <section id="about" className="about-section">
-            <SectionHeader text="about us" />
-            <p className="about--tagline">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eaque.</p>
-            <div className="about__content-container">
-                <Mission title="our mission" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium" />
-                <AboutImage/>
-            </div>
-            <div className="about__content-container">
-                     <Mission title="our vision" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium" />
-                <div style={{order:-1 }}>
-                    <AboutImage/>
-                </div>
-            </div>
-        </section>
-        <Footer/>
-    </div>);
-}
- 
+      </section>
+      <section className="product-section">
+        <SectionHeader text="Products" />
+        <div className="product-container">
+          {getHighlightProduct().map((p) => {
+            return (
+              <Product
+                key={p._id}
+                handleClick={() => handleClick(p._id)}
+                image={p.image}
+                name={p.name}
+                price={p.price}
+                showPrice={true}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <section id="about" className="about-section">
+        <SectionHeader text="about us" />
+        <p className="about--tagline">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+          eaque.
+        </p>
+        <div className="about__content-container">
+          <Mission
+            title="our mission"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium"
+          />
+          <AboutImage />
+        </div>
+        <div className="about__content-container">
+          <Mission
+            title="our vision"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium"
+          />
+          <div style={{ order: -1 }}>
+            <AboutImage />
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
 export default HomePage;
---
+++ src/utils/products.js
@@ -0,0 +1,39 @@
+const products=[
+    { _id: 1,name:"Tile cement",price:230,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 2,name:"Chisel",price:830,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 3,name:"Hammer",price:90,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 4,name:"Iron rods",price:120,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 5,name:"Pavement blocks",price:300,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 6,name:"master lock",price:600,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 7,name:"Azar oil paint",price:200,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 8,name:"Tape measure",price:240,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 9,name:"binding wire",price:350,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 10,name:"metal door",price:400,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 11,name:"Nails",price:213,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" },
+    { _id: 12,name:"brush",price:40,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus aut quidem officiis reprehenderit. Aliquid quis similique facilis aliquam dolor.",image:"https://picsum.photos/320/320" }
+]
+
+const hotProducts = [
+     { _id: 1,name:"Wall tiles",price:120,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus",image:"https://picsum.photos/320/320" },
+     { _id: 2,name:"emulsion paint",price:200,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus",image:"https://picsum.photos/320/320" },
+     { _id: 3,name:"azar oil paint",price:1300,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus",image:"https://picsum.photos/320/320" },
+     { _id: 4,name:"aluminium windows",price:500,description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus",image:"https://picsum.photos/320/320" },
+]
+
+export  function getAllProducts() {
+    return [...products]
+}
+
+ export function getProductById(id) {
+    const product = products.find(p => p._id === id)
+    return product
+}
+
+export function getHotProducts() {
+    return [...hotProducts]
+}
+
+
+export function getHighlightProduct() {
+    return products.slice(2,6)
+}
