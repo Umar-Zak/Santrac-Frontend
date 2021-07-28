0c32c3eafc1f6edeaa7572a9029c0a65c77a52df
umar-zak <umarabanga78@gmail.com>
2021-07-28 05:42:51 -0800

    fix layout issues on product form page

--- src/pages/ProductFormPage.jsx
+++ src/pages/ProductFormPage.jsx
@@ -59,94 +59,103 @@ const ProductForm = () => {
   }, []);
 
   return (
-    <div className="block">
+    <div>
       <Navbar />
-      <div className="form-container">
-        <SectionHeader text="Product Form" />
-        <Formik
-          onSubmit={async (values) => {
-            setSpiner(true);
-            const url = await uploadFile(image);
-            try {
-              await addProduct({ ...values, image: url });
-              history.push("/dashboard");
-            } catch ({ response: { data, status } }) {
-              setSpiner(false);
-              if (status < 400) return toast(data);
-
-              toast("Unexpected error. Please try again");
-            }
-          }}
-          validationSchema={schema}
-          initialValues={{
-            name: "",
-            price: "",
-            description: "",
-            quantity: "",
-            category: "",
-          }}
-        >
-          {({ touched, handleChange, errors,values }) => (
-            <Form>
-              {touched.name && errors.name && <Error text={errors.name} />}
-              <Input
-                name="name"
-                placeholder="Product name"
-                handleChange={handleChange}
-                type="text"
-              />
-              {touched.price && errors.price && <Error text={errors.price} />}
-              <Input
-                name="price"
-                placeholder="Product price"
-                handleChange={handleChange}
-                type="number"
-              />
-              {touched.quantity && errors.quantity && (
-                <Error text={errors.quantity} />
-              )}
-              <Input
-                name="quantity"
-                placeholder="Quantity"
-                handleChange={handleChange}
-                type="number"
-              />
-              {touched.category && errors.category && (
-                <Error text={errors.category} />
-              )}
-              <select onChange={handleChange} className="input" name="category">
-                <option value="">Choose category</option>
-                {categories.map(({ name, _id }) => (
-                  <option value={_id} key={_id}>
-                    {name}
-                  </option>
-                ))}
-              </select>
-              {touched.description && errors.description && (
-                <Error text={errors.description} />
-              )}
-              <textarea
-                onChange={handleChange}
-                className="input"
-                name="description"
-                id=""
-                cols="30"
-                rows="5"
-                placeholder="Description"
-              ></textarea>
-              <Input name="image" handleChange={handleImageInput} type="file" />
-              {spiner && <Spiner />}
-              {!spiner && (
-                <Button
-                  text="Add Product"
-                  type="button--primary button--large"
+      <div className="block">
+        <div className="form-container">
+          <SectionHeader text="Product Form" />
+          <Formik
+            onSubmit={async (values) => {
+              setSpiner(true);
+              const url = await uploadFile(image);
+              try {
+                await addProduct({ ...values, image: url });
+                history.push("/dashboard");
+              } catch ({ response: { data, status } }) {
+                setSpiner(false);
+                if (status < 400) return toast(data);
+                toast("Unexpected error. Please try again");
+              }
+            }}
+            validationSchema={schema}
+            initialValues={{
+              name: "",
+              price: "",
+              description: "",
+              quantity: "",
+              category: "",
+            }}
+          >
+            {({ touched, handleChange, errors, values }) => (
+              <Form>
+                {touched.name && errors.name && <Error text={errors.name} />}
+                <Input
+                  name="name"
+                  placeholder="Product name"
+                  handleChange={handleChange}
+                  type="text"
                 />
-              )}
-            </Form>
-          )}
-        </Formik>
+                {touched.price && errors.price && <Error text={errors.price} />}
+                <Input
+                  name="price"
+                  placeholder="Product price"
+                  handleChange={handleChange}
+                  type="number"
+                />
+                {touched.quantity && errors.quantity && (
+                  <Error text={errors.quantity} />
+                )}
+                <Input
+                  name="quantity"
+                  placeholder="Quantity"
+                  handleChange={handleChange}
+                  type="number"
+                />
+                {touched.category && errors.category && (
+                  <Error text={errors.category} />
+                )}
+                <select
+                  onChange={handleChange}
+                  className="input"
+                  name="category"
+                >
+                  <option value="">Choose category</option>
+                  {categories.map(({ name, _id }) => (
+                    <option value={_id} key={_id}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+                {touched.description && errors.description && (
+                  <Error text={errors.description} />
+                )}
+                <textarea
+                  onChange={handleChange}
+                  className="input"
+                  name="description"
+                  id=""
+                  cols="30"
+                  rows="5"
+                  placeholder="Description"
+                ></textarea>
+                <Input
+                  name="image"
+                  handleChange={handleImageInput}
+                  type="file"
+                />
+                {spiner && <Spiner />}
+                {!spiner && (
+                  <Button
+                    text="Add Product"
+                    type="button--primary button--large"
+                  />
+                )}
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <div className="divider"></div>
       </div>
-      <div className="divider"></div>
       <Footer />
     </div>
   );
